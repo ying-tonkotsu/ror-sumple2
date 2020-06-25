25db882657d54e880697d195ee4d5d74e8115295
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-25 14:29:37 +0900

    ユーザー一覧にユーザを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     # ユーザー一覧
     def index
-
+        @users = User.all
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # ユーザー一覧
+  get '/users/index' => 'users#index'
+
+
   # サイトマップへ飛ばす
   get '/' => 'top#sitemap'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
